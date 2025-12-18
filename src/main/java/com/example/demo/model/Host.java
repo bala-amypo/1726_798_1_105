8ee35151cbe71f9b40a1f20bcb21e82177a7c9d8
bala-amypo <bala@amypo.in>
2025-12-18 07:48:03 +0000

    User commit

---
+++ src/main/java/com/example/demo/model/Host.java
@@ -0,0 +1,107 @@
+package com.example.demo.model;
+
+import jakarta.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(
+    name = "hosts",
+    uniqueConstraints = @UniqueConstraint(columnNames = "email")
+)
+public class Host {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String hostName;
+
+    private String fullname;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String department;
+
+    @Column(nullable = false)
+    private String phone;
+
+    private LocalDateTime createdAt;
+
+    @PrePersist
+    public void onCreate() {
+        this.createdAt = LocalDateTime.now();
+    }
+
+    public Long getId() {
+        return id;
+    }
+public Host(){
+
+}
+    public Host( String hostName, String fullname, String email, String department, String phone) {
+       
+        this.hostName = hostName;
+        this.fullname = fullname;
+        this.email = email;
+        this.department = department;
+        this.phone = phone;
+        
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getHostName() {
+        return hostName;
+    }
+
+    public void setHostName(String hostName) {
+        this.hostName = hostName;
+    }
+
+    public String getFullname() {
+        return fullname;
+    }
+
+    public void setFullname(String fullname) {
+        this.fullname = fullname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    
+}
