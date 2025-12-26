a682853d260b1beff0f357330896e99503aea5bd
bala-amypo <bala@amypo.in>
2025-12-26 05:13:30 +0000

    User commit

--- src/main/java/com/example/demo/repository/AlertNotificationRepository.java
+++ src/main/java/com/example/demo/repository/AlertNotificationRepository.java
@@ -1,10 +1,12 @@
 package com.example.demo.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import com.example.demo.model.AlertNotification;
 
-@Repository
 public interface AlertNotificationRepository extends JpaRepository<AlertNotification, Long> {
+
+    List<AlertNotification> findByVisitLogId(Long visitLogId);
 }
--- src/main/java/com/example/demo/repository/VisitLogRepository.java
+++ src/main/java/com/example/demo/repository/VisitLogRepository.java
@@ -3,12 +3,14 @@ package com.example.demo.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import com.example.demo.model.VisitLog;
 
-@Repository
 public interface VisitLogRepository extends JpaRepository<VisitLog, Long> {
 
     List<VisitLog> findByCheckOutTimeIsNull();
+
+    List<VisitLog> findByVisitorId(Long visitorId);
+
+    List<VisitLog> findByHostId(Long hostId);
 }
